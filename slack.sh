6b131015dd1a2dbd547365cb9d8edae30abe5081
TSURU <tsuyoshi.tsurushima@gmail.com>
2015-02-26 12:30:40 +0900

    Slack AlertScript

--- slack.sh
+++ slack.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
+url='https://hooks.slack.com/services/XXX/XXX/XXX'
+username='Zabbix'
+icon_url='https://github.com/ttsuru/zabbix-slack/raw/gh-pages/icon.png'
+
+to="$1"
+subject="$2"
+stat=`echo $subject | cut -d ":" -f 1`
+
+if [ $stat = 'OK' ]; then
+    color='good'
+elif [ $stat = 'PROBLEM' ]; then
+    color='danger'
+else
+    color=''
+fi
+
+message="${subject}"
+
+if [ "$3" != "" ]
+then
+    fields=", \"fields\": $3"
+else
+    fields=""
+fi
+
+
+payload="payload={\"channel\": \"${to}\", \"username\": \"${username}\", \"text\": \"${message}\", \"color\": \"${color}\", \"icon_url\": \"${icon_url}\"${fields}}"
+curl -m 5 --data-urlencode "${payload}" $url
 
